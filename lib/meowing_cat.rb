087ff6861f90180550c4196100609899d88bd1d7
davidamato8861 <davida8861@hstat.org>
2016-12-20 21:10:50 +0000

    finish hw

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,8 @@
 ## code your solution here. 
+class Cat #instantiates a new Cat
+attr_accessor :name #combines Setter and Getter methods for name of new Cat 
+
+def meow #puts meow when a new instance of Cat is called
+    puts "meow!"
+end
+end
